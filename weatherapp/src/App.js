b79b6e13b336525264308dce833480dd19902228
ihab-boudargham <ihab.boudargham@hotmail.com>
2023-12-23 13:00:50 +0200

    Sections

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Search from './components/Search';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App relative">
 
+        <img className='background object-cover' src = "/4.jpg"/>
+        <Search />
+        <Form />
     </div>
-  );
-}
+  )
+  }
 
 export default App;
